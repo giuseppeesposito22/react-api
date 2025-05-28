eb6baea905300acdfc729399266b78e1a217c032
giuseppeesposito22 <gius.espo22@gmail.com>
2025-05-28 17:02:32 +0200

    render actor and actress in page

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,47 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import ActressesCard from "./components/ActressesCard";
+import Actorcard from "./components/ActorCard";
 
 function App() {
   const actressesUrl = "https://lanciweb.github.io/demo/api/actresses/";
+  const actorUrl = "https://lanciweb.github.io/demo/api/actors/";
 
   const [actresses, setActresses] = useState([]);
+  const [actors, setActors] = useState([]);
+  const [render, setRender] = useState("");
 
   const fetchActresses = () => {
     axios.get(actressesUrl).then((res) => setActresses(res.data));
+    setRender("Actress");
+  };
+
+  const fetchActors = () => {
+    axios.get(actorUrl).then((res) => setActors(res.data));
+    setRender("Actor");
   };
 
   useEffect(fetchActresses, []);
 
   return (
     <>
-      <h1 className="text-center">Actress API</h1>
-      <div className="container my-5">
+      <h1 className="text-center">Actors API</h1>
+
+      <div className="d-flex justify-content-center my-4">
+        <button className="mx-5 btn btn-primary" onClick={fetchActresses}>
+          Mostra Attrici
+        </button>
+        <button className="mx-5 btn btn-primary" onClick={fetchActors}>
+          Mostra Attori
+        </button>
+      </div>
+      <div className="container my-5 ">
         <div className="row row-cols-4 g-4">
-          {actresses.map((act) => (
-            <ActressesCard key={act.id} act={act} />
-          ))}
+          {render === "Actress" &&
+            actresses.map((act) => <Actorcard key={act.id} act={act} />)}
+        </div>
+        <div className="row row-cols-4 g-4">
+          {render === "Actor" &&
+            actors.map((act) => <Actorcard key={act.id} act={act} />)}
         </div>
       </div>
     </>
